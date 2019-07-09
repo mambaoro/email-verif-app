bfa9ba455568a52898d856dafcb97b2929f34ba8
Baoro <mam.baoro@outlook.fr>
2019-07-09 11:10:30 +0200

    ready for deployment

--- app/components/Form/index.js
+++ app/components/Form/index.js
@@ -14,6 +14,80 @@ import axios from 'axios';
 import { emailFormValidationSchema } from './validationSchema';
 import { cssSharedBox } from '../../base';
 
+const cssBoxShadow = css`
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+  :hover {
+    box-shadow: 0 3px 4px rgba(0, 0, 0, 0.32);
+  }
+`;
+
+const Section = styled.section`
+  ${cssSharedBox}
+  span {
+    display: block;
+    color: crimson;
+    margin-top: 1rem;
+    color: #420d09;
+    &:first-of-type {
+      margin-top: 2.5rem;
+    }
+  }
+  span.accepted {
+    color: #fafafa;
+  }
+  transition: height 1s ease-in-out;
+`;
+
+const DivInputs = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  align-items: center;
+  div {
+    &:first-of-type {
+      margin-bottom: 2rem;
+    }
+  }
+  label {
+    margin-bottom: 1rem;
+    border-bottom: 1px solid #fafafa;
+  }
+  input {
+    display: inline-block;
+    border-radius: 5px;
+    padding: 1.225rem;
+    min-width: 25rem;
+    border: 2px solid var(--color-h1);
+    background-color: #fafafa;
+    ::placeholder {
+      text-align: center;
+    }
+    ${cssBoxShadow}
+  }
+`;
+
+const Button = styled.button`
+  margin-top: 2.5rem;
+  text-align: center;
+  border: none;
+  padding: 1.5rem 3rem;
+  border-radius: 6px;
+  color: var(--bg-color-box);
+  background-color: var(--color-special-1);
+  font-size: 1.6rem;
+  font-weight: bold;
+  ${cssBoxShadow}
+  cursor: pointer;
+`;
+
+const DivSpinner = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2.5rem auto;
+  margin-bottom: 0;
+`;
+
 function Form() {
   const [isVerified, setIsVerified] = useState(false);
   const [isAccepted, setIsAccepted] = useState(false);
@@ -88,80 +162,6 @@ function Form() {
     </Section>
   );
 }
-
-const cssBoxShadow = css`
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
-  :hover {
-    box-shadow: 0 3px 4px rgba(0, 0, 0, 0.32);
-  }
-`;
-
-const Section = styled.section`
-  ${cssSharedBox}
-  span {
-    display: block;
-    color: crimson;
-    margin-top: 1rem;
-    color: #420d09;
-    &:first-of-type {
-      margin-top: 2.5rem;
-    }
-  }
-  span.accepted {
-    color: #fafafa;
-  }
-  transition: height 1s ease-in-out;
-`;
-
-const DivInputs = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  align-items: center;
-  div {
-    &:first-of-type {
-      margin-bottom: 2rem;
-    }
-  }
-  label {
-    margin-bottom: 1rem;
-    border-bottom: 1px solid #fafafa;
-  }
-  input {
-    display: inline-block;
-    border-radius: 5px;
-    padding: 1.225rem;
-    min-width: 25rem;
-    border: 2px solid var(--color-h1);
-    background-color: #fafafa;
-    ::placeholder {
-      text-align: center;
-    }
-    ${cssBoxShadow}
-  }
-`;
-
-const Button = styled.button`
-  margin-top: 2.5rem;
-  text-align: center;
-  border: none;
-  padding: 1.5rem 3rem;
-  border-radius: 6px;
-  color: var(--bg-color-box);
-  background-color: var(--color-special-1);
-  font-size: 1.6rem;
-  font-weight: bold;
-  ${cssBoxShadow}
-  cursor: pointer;
-`;
-
-const DivSpinner = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 2.5rem auto;
-  margin-bottom: 0;
-`;
 
 Form.propTypes = {};
 
